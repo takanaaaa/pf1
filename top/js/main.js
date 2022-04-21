84b1498441b35f07bf67cfe784fcbb4111f1be15
training220325 <ys.training220325@gmail.com>
2022-04-21 21:44:21 +0900

    [Add]css/js

---
+++ top/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+  // var rellax = new Rellax('.header', {
+  //   speed: 2,
+  //   center: false,
+  //   wrapper: null,
+  //   round: true,
+  //   vertical: true,
+  //   horizontal: false
+  // });
+
+
